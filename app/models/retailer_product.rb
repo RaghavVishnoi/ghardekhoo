f35b8ac7b845c839f0519338b47b663f28eaf5c8
Raghav Pratap Singh <raghav.pratap@trantorinc.com>
2019-01-28 00:00:58 +0530

    Add default value for retailer product

--- app/models/retailer_product.rb
+++ app/models/retailer_product.rb
@@ -3,6 +3,8 @@ class RetailerProduct < ApplicationRecord
 	belongs_to :product_sub_category
 	has_many :retailer_product_photos, dependent: :destroy
 
+	before_create :default_value
+
 	attr_accessor :photos
 
 	def status_enum
@@ -11,6 +13,10 @@ class RetailerProduct < ApplicationRecord
 
 	def name
 		"#{product_name} - #{sku_code}"
+	end
+
+	def default_value
+		self.status = 0
 	end
 	
 end
