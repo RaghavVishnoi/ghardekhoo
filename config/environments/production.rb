77a9334b6397b022316cc0757ea5341c1a5a80bf
RaghavVishnoi <raghavvishnoi10@gmail.com>
2021-01-14 23:02:00 +0530

    Update production.rb
    Updated url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.default_url_options = { host: 'https://ghardekhoo-staging.herokuapp.com' }
+  config.action_controller.default_url_options = { host: 'http://ghardekhoo.com' }
 
   # SMTP settings for gmail
   config.action_mailer.delivery_method = :smtp
